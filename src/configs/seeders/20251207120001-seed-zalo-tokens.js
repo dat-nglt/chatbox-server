6dc242f8aa3cab5810c87c89563beb4caf3f38f2
Darren <nguyenletandat.contact@gmail.com>
2025-12-15 08:17:20 +0700

    Bóc tách các module Zalo Service thành các file riêng biệt

--- src/configs/seeders/20251207120001-seed-zalo-tokens.js
+++ src/configs/seeders/20251207120001-seed-zalo-tokens.js
@@ -4,9 +4,9 @@ export const up = async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("zalo_tokens", [
         {
             access_token:
-                "Dq-JJ-Ohm2asJf50uoJAA3P0c4peF9LDDpsZLhmbzM8HJk1rZs_JUZXHY4BiOgznEc--5AzEiYy3J9KUb7d7EWDSmWIEJ-e4ML_CRSriubDzKTbpz4_aMofCmKMXQPeX6NgNAwblkWq_IUmPhthF901A-Z-eSUWOTqVQ7-D7wonHHUWc_KtC02eiboRWCBi7DWYZ7ubhcoezKhqExNRdCJbFz02lSDexBHkB09WOtnmDVCu0idxB0WPv-ZEDTDqq1X77KPi7baLCBRbjtnIBVsincbxZ58XDQZw1UTGRhmH44xycyJMUCtjKfGAfTOW134A_OAbLYcSUQ8HQo5IeI5naYsZoOznPKGJCME4Lvru_DSuzZJRUN3uEqbkT58Hz4dBMJjXQWIjiNPiAm3lB51OhmHvBGUTIOb3aCy0f",
+                "d0AJ2gJwgZoT0v45eVMWFPqDc0cgxVH-a62_7zpby5BWVOyAbDt8PgvgWmtomCfKg7ssET3gttoH7O4tqRd20OOThLRfuk86qcAeHQlqwN3Y5xWzZkZNVz1ZxZ6OshL3e43D6i3FaN-WSzW9pSwIEeHpy0cpYvL_s1VGIfEoddZs7leLYk2sBFPqxLgw-RCuX5xHTkVVknsGO_1NxUQ_7AL5w5BioTKhqtQIT8_Nsn3EBhPWXDZD2TbizKAmyRSOrbJTBOlDXagmSzuyy-Y23RHFxaJBzzOoe6wDPid2mGJ_VgTtmFFM8_zrZa6WuxykmaljPldqfb32QzuLW-oIO_nOt5E6uR8Ps7hOAfNtvWVDVDTreEsKCkr0ysQxoSWaZMIGHSx4wtQoPvCPy_YhBPXiv4hqmTiT3iu80ARYgZm",
             refresh_token:
-                "H6Yk9S7rOK55QQmgnFfhG4LdfIRFuW4o1YQPCyV04WurA8PgiU00TaSUhMAvmnzoEHwlH-_4CcqCAxjLXkWgJb8dYocuwm1AQ2ooMhFC1rT6BBjYcF4nSrape4QzqGTbJ3M11gh1631-8zO8ah5j7495eWsre2W0GNoqEA6pE1f8UfaVghuzVIjZZst9Z2TzLtQSMA-K2NrrQePAeu81K28SYLN--W5gFJ38NVRs3ca8Eh5jtRmuS1Swjrlpr2iy3JUA9_Js139gAxSXlU479q0PoHwAyL8PI17r3exlRMjSCCOCjjro9dOws0ECm5O_UHZW0-VPVpWE5B4rZUOP8tC4WI6CWWaUSrkGTPN93s0LJyTVpQDEM3ynWbNKmonjBWwRJ_Z05LG_NwKsiwl-KiFjOK4",
+                "xP9o4G5nTa3iat0U17vyGUoTUH8FPdj5XliWHHv_N12f-ID7CXb1J96-8aW7MbDLcC8NPNPW47FPtdn6Tr86Jjpf04PPKrboszKy9qbtM3VQqa0SKpKz2R6f9GLSFrPJw8Ce0tSlQ0pKfGqOU3zf4vM4AJWkFauyk8uVFGPfO1QkzmS3FLLV3wpQAIybN5uWgS8V7GbZ622rzLuk9KOwFRJYMoW_U0OAdzTbNYDc0q-HwrbB26qYP8hKOpiMQsKMdzm15GixKZ6ProOSCL92A9Zt6HS6L6yyhTOxOq1IGpJqppCmOrHqCjFMSGrDDt8Wniy_02juCo--yZyWFHz58PQe9JS8BtWdg98uSaC9HNhKhnbLG3LYQFIs5MSTMtKvgSC60KeZLW6bcYWnsjKrPmDfTa0",
             access_token_expires_at: new Date(Date.now() + 3600000), // 1 hour from now
             refresh_token_expires_at: new Date(Date.now() + 7776000000), // 3 tháng từ now
             created_at: new Date(),
